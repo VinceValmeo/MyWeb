6e10ec017428139966af75ba0bd2586183bd150b
Vnc <vivalmeo@gmail.com>
2022-08-02 05:40:10 +0800

    Solved: Multiplication Table

--- testJava.java
+++ testJava.java
@@ -1,9 +1,10 @@
 public class testJava {
     public static void main(String[] args) {
-        for (int i = 1; i != 10; i++) {
-            for (int j = 1; j != 10; ++j)
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; ++j) {
                 System.out.print((i * j) + "\t");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 }
